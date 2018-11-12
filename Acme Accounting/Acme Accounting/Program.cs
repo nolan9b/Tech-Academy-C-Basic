0c7745cd37b59d0bd3a249c8edbddf287591720f
nolan <nolan9b@outlook.com>
2018-11-12 13:05:35 +0800

    Commiting Acme Accounting

---
+++ Acme Accounting/Acme Accounting/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Acme_Accounting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remember, we're \"accounting\" on you!");
+            Console.Read();
+        }
+    }
+}
