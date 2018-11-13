85e17c1c743ee0d02a8b94b9c5826c616d91d680
nolan <nolan9b@outlook.com>
2018-11-13 19:51:29 +0800

    Loop Exercise

---
+++ While Example/While Example/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isQuit = false;
+            Console.WriteLine("While Loop...");
+            while (!isQuit)
+            {
+                Console.WriteLine("Do you want to quit?");
+                string answer = Console.ReadLine();
+
+                if (answer == "yes")
+                    isQuit = true;
+            }
+
+            isQuit = false;
+            Console.WriteLine("Now for the Do While....");
+            do
+            {
+                Console.WriteLine("Do you want to quit?");
+                string answer = Console.ReadLine();
+
+                if (answer == "yes")
+                    isQuit = true;
+            }
+            while (!isQuit);
+        }
+    }
+}
