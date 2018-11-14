027eb2c600a63c346f53bacf32dcb6675885b35e
nolan <nolan9b@outlook.com>
2018-11-14 21:27:22 +0800

    Commit Class Exercise

---
+++ Class Exercise/Class Exercise/Calculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Exercise
+{
+    class Calculator
+    {
+
+        public int AddTwenty(int num)
+        {
+            return num + 20;
+        }
+
+        public int MinusTwenty(int num)
+        {
+            return num - 20;
+        }
+
+        public int TimesTwenty(int num)
+        {
+            return num * 20;
+        }
+    }
+}
---
+++ Class Exercise/Class Exercise/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+
+            Console.WriteLine("What number do you want to do maths on?");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("The number add twenty is: " + calc.AddTwenty(number));
+            Console.WriteLine("The number minus twenty is: " + calc.MinusTwenty(number));
+            Console.WriteLine("The number times twenty is: " + calc.TimesTwenty(number));
+
+            Console.Read();
+        }
+    }
+}
