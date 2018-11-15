b66340966994db870f5a712157d052c05449ed30
nolan <nolan9b@outlook.com>
2018-11-15 10:26:39 +0800

    Commit  Overload exercise

---
+++ MethodTest2/MethodTest2/Method Test.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodTest2
+{
+    class Method_Test
+    {
+        public int DoMath(int num)
+        {
+            return num + 20;
+        }
+
+        public int DoMath(decimal num)
+        {
+            return Convert.ToInt32(num / 202.25m);
+        }
+
+        public int DoMath(string num)
+        {
+            return Convert.ToInt32(num) * 20;
+        }
+    }
+}
---
+++ MethodTest2/MethodTest2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodTest2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Method_Test mt = new Method_Test();
+
+            int intVar = 12;
+            decimal decVar = 1618;
+
+            Console.WriteLine("12 + 20 is: " + mt.DoMath(intVar));
+            Console.WriteLine("1618 / 202.25 is: " + mt.DoMath(decVar));
+            Console.WriteLine("Converting \"8\" to int and times by 20: " + mt.DoMath("8"));
+
+            Console.ReadLine();
+        }
+    }
+}
