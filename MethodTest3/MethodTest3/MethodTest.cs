39078c608d66c987263b7b71b2be8e79d6cf02e7
nolan <nolan9b@outlook.com>
2018-11-15 10:54:43 +0800

    Commit Optional Paramters Exercise

---
+++ MethodTest3/MethodTest3/MethodTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodTest3
+{
+    class MethodTest
+    {
+        public int DoMaths(int num1, int num2 = 100)
+        {
+            return num1 * num2;
+        }
+    }
+}
---
+++ MethodTest3/MethodTest3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodTest3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MethodTest mt = new MethodTest();
+
+            Console.Write("Please enter first number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = 0;
+            bool isDefault = false;
+
+            try
+            {
+                Console.Write("Please enter second number (optional): ");
+                num2 = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("No second number entered using default...");
+                isDefault = true;
+            }
+
+            Console.WriteLine((isDefault) ? mt.DoMaths(num1): mt.DoMaths(num1,num2));
+
+            Console.ReadLine();
+        }
+    }
+}
