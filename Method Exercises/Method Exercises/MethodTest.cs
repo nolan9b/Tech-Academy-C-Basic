6fea78c8c7b8b8a5521af3894ef2ae5d61cdfc0e
nolan <nolan9b@outlook.com>
2018-11-15 08:06:22 +0800

    Commit Method Exercies

---
+++ Method Exercises/Method Exercises/MethodTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Exercises
+{
+    class MethodTest
+    {
+        public void Sum(int num1, int num2)
+        {
+            num1 = num1 + 20;
+
+            Console.WriteLine("This is num2: {0}", num2);
+
+        }
+    }
+}
---
+++ Method Exercises/Method Exercises/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MethodTest mt = new MethodTest();
+
+            mt.Sum(20, 30);
+
+            mt.Sum(num1: 40, num2: 50);
+
+            Console.ReadLine();
+        }
+
+        
+    }
+}
