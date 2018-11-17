2aefcc925f4ee9fd13e7fd7732c1400cc87c081b
nolan <nolan9b@outlook.com>
2018-11-17 20:24:21 +0800

    Operator Overload Exercise

---
+++ Operator Overload/Operator Overload/Employee.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator_Overload
+{
+    public class Employee : Person, IQuitabble
+    {
+        public int Id { get; set; }
+
+        public override void SayName()
+        {
+            Console.WriteLine(FirstName + " " + LastName);
+        }
+
+        public void Quit()
+        {
+            Console.WriteLine("I Quit");
+        }
+
+        public static bool operator==(Employee firstEmployee, Employee secondEmployee)
+        {
+            if (firstEmployee.Id == secondEmployee.Id)
+            {
+                return true;
+            }
+            else if (firstEmployee.Id != secondEmployee.Id)
+            {
+                return false;
+            }
+
+            return false;
+        }
+        public static bool operator!=(Employee firstEmployee, Employee secondEmployee)
+        {
+            if (firstEmployee.Id != secondEmployee.Id)
+            {
+                return true;
+            }
+            else if (firstEmployee.Id == secondEmployee.Id)
+            {
+                return false;
+            }
+
+            return false;
+        }
+
+    }
+}
