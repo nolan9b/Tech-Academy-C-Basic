0a7e967244abeabbac32385eb2f5c35b0d936980
nolan <nolan9b@outlook.com>
2018-11-17 19:38:09 +0800

    Interface Exercise

---
+++ Interface/Interface/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee() { FirstName = "Sample", LastName = "Student" };
+            employee.SayName();
+
+            IQuittable iQuit = new Employee();
+
+            iQuit.Quit();
+
+            Console.ReadLine();
+        }
+    }
+}
