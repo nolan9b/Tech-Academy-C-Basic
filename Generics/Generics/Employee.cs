e2ba4f43ef43d2efa704e4252ef6a436d87f23c9
nolan <nolan9b@outlook.com>
2018-11-18 15:38:23 +0800

    Commit Generic Exercise

---
+++ Generics/Generics/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class Employee<T> : Person, IQuitabble
+    {
+        public int Id { get; set; }
+        public List<T> Things { get; set; }
+
+        public override void SayName()
+        {
+            Console.WriteLine(FirstName + " " + LastName);
+        }
+
+        public void Quit()
+        {
+            Console.WriteLine("I Quit");
+        }
+
+        
+
+    }
+}
---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> stringEmployee = new Employee<string>();
+            stringEmployee.Things = new List<string>();
+
+            Employee<int> intEmployee = new Employee<int>();
+            intEmployee.Things = new List<int>();
+
+            stringEmployee.Things.Add("Peter");
+            stringEmployee.Things.Add("Wendy");
+            stringEmployee.Things.Add("Tinkerbell");
+            stringEmployee.Things.Add("Hook");
+
+            intEmployee.Things.Add(1);
+            intEmployee.Things.Add(2);
+            intEmployee.Things.Add(3);
+            intEmployee.Things.Add(4);
+
+            foreach(string thing in stringEmployee.Things)
+            {
+                Console.WriteLine(thing);
+            }
+
+            foreach (int intThing in intEmployee.Things)
+            {
+                Console.WriteLine(intThing);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
