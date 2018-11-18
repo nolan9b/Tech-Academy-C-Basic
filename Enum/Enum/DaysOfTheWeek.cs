01e3b8c9f9dfc2bed75facf76fda14ed6ca13710
nolan <nolan9b@outlook.com>
2018-11-18 21:05:07 +0800

    Commit Enum Exercise

---
+++ Enum/Enum/DaysOfTheWeek.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumExercise
+{
+    class DaysOfTheWeek
+    {
+        public DayOfTheWeek days { get; set; }
+    }
+
+    public enum DayOfTheWeek
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+}
---
+++ Enum/Enum/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumExercise
+
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            DaysOfTheWeek today = new DaysOfTheWeek();
+
+
+            Console.WriteLine("Please enter the current day of the week");
+            string dayEntered = Console.ReadLine();
+            DayOfTheWeek output = new DayOfTheWeek();
+
+            try
+            {
+                output = (DayOfTheWeek)Enum.Parse(typeof(DayOfTheWeek), dayEntered);
+            }
+            
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+
+            
+            Console.ReadLine();
+        }
+
+        
+    }
+}
