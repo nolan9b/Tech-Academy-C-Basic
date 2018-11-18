b8a2b3277658f8fe6217b11d3d4d20fa53b6803d
nolan <nolan9b@outlook.com>
2018-11-18 21:22:37 +0800

    Commit Struct Exercise

---
+++ StructExercise/StructExercise/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number num = new Number();
+
+            num.Amount = 100.50m;
+
+            Console.WriteLine(num.Amount);
+            Console.ReadLine();
+        }
+    }
+}
