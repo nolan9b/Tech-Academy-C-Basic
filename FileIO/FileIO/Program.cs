0d5aca9e6ab8e4f770694a55e63be300a9ca5073
nolan <nolan9b@outlook.com>
2018-11-19 13:22:28 +0800

    Commit File IO Exercise

---
+++ FileIO/FileIO/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            File.WriteAllText(@"D:\logs\number.txt", number.ToString());
+
+            Console.WriteLine("The number is: {0}", File.ReadAllText(@"D:\logs\number.txt"));
+            Console.ReadLine();
+        }
+    }
+}
