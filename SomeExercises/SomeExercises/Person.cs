26126425df100556fa019f10ed9f62f2369ff44b
nolan <nolan9b@outlook.com>
2018-11-19 21:33:03 +0800

    Exercises Part 1

---
+++ SomeExercises/SomeExercises/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomeExercises
+{
+    class Person
+    {
+        public Person(string name)
+        {
+            Name = name;
+        }
+
+        public Person(string name, int number) : this(name)
+        {
+            Telephone = number;
+        }
+
+        public int Telephone { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ SomeExercises/SomeExercises/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomeExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string programName = "C Sharp Exercises";
+            Console.WriteLine(programName);
+
+            var number = 12345;
+            Console.WriteLine("The number is: {0}", number);
+
+            var person1 = new Person("Peter");
+            var person2 = new Person("Pan", 12345);
+
+            Console.WriteLine("{0},{1}",person1.Name, person1.Telephone);
+            Console.WriteLine("{0},{1}", person2.Name, person2.Telephone);
+
+            Console.ReadLine();
+        }
+    }
+}
