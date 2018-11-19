2d37783d88f054d7c4c8d5b06fcbc1ab2b2860bb
nolan <nolan9b@outlook.com>
2018-11-19 13:35:39 +0800

    Commit DateTime Exercise

---
+++ DateTimeExercise/DateTimeExercise/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+
+            Console.WriteLine("Please enter a number");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            DateTime currentTime = DateTime.Now;
+
+            TimeSpan timeSpan = new TimeSpan(0, number, 0, 0);
+            DateTime newTime = currentTime.Add(timeSpan);
+
+            Console.WriteLine(newTime);
+
+            Console.ReadLine();
+
+        }
+    }
+}
